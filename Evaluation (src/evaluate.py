535d43fdc9609b3d5afab59f3dbda7edef729135
vinotha2007-V <logiravi2002@gmail.com>
2026-05-01 17:45:18 +0530

    Create evaluate.py

---
+++ Evaluation (src/evaluate.py
@@ -0,0 +1,7 @@
+from sklearn.metrics import classification_report, confusion_matrix
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
